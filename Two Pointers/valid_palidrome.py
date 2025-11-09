c9f4b7d242dc478dcc325ea412efcda7cae66a45
sameera03 <71255204+sameera03@users.noreply.github.com>
2025-11-09 07:33:13 +0530

    Implement palindrome check using two pointers

---
+++ Two Pointers/valid_palidrome.py
@@ -0,0 +1,17 @@
+def isPalindrome(s: str) -> bool:
+      l, r = 0, len(s)-1
+      s = s.lower()
+      while l < r:
+          if not s[l].isalnum():
+              l += 1
+              continue
+          if not s[r].isalnum():
+              r -= 1
+              continue
+          if s[l] == s[r]:
+              l += 1
+              r -= 1
+          else:
+              return False
+      return True
+        
