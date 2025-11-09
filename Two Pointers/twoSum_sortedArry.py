9998047c059012829fe4cc684b0daf397b0f65cc
sameera03 <71255204+sameera03@users.noreply.github.com>
2025-11-09 07:41:32 +0530

    Add twoSum function for sorted array
    Implement twoSum function using two pointers technique.

---
+++ Two Pointers/twoSum_sortedArry.py
@@ -0,0 +1,9 @@
+def twoSum(nums: List[int], target: int) -> List[int]:
+      l, r = 0, len(nums)-1
+      while l < r:
+          if (nums[l] + nums[r]) > target:
+              r -= 1
+          elif (nums[l] + nums[r]) < target:
+              l += 1
+          else:
+              return [l,r]
